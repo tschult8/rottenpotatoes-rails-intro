d2c38a5dad3e371ff4a484a74e842eab1ea02f86
Tyler Schultz <tschult8@uncc.edu>
2018-03-21 01:34:28 +0000

    Done with part 0

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809022253) do
-
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.string   "rating"
-    t.text     "description"
-    t.datetime "release_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
